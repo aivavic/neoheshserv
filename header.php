46bc154694f7f02fcc6b375298083d5404673e7b
aivavic <afanasjev-v@yandex.ru>
2016-09-27 22:04:31 +0300

    add menu

--- header.php
+++ header.php
@@ -9,25 +9,19 @@ wp_head();
 ?>
 <header>
     <div class="container">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.jpg" alt="logo" id="logo">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.jpg" alt="logo" id="logo">
+    </div>
     <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <?php
-            wp_nav_menu(array('theme_location' => 'header-menu',
-                'container_class' => 'navbar-header',
-                'items_wrap' => '<ul id="%1$s" class="nav navbar-nav">%3$s</ul>',
-            ));
-            ?>
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+
+        <?php
+        wp_nav_menu(array('theme_location' => 'header-menu',
+            'container_class' => 'header-menu',
+            'items_wrap' => '<ul id="%1$s" class="nav navbar-nav">%3$s</ul>',
+        ));
+        ?>
 
         </div>
-        </div>
+
     </nav>
-    </div>
+
 </header>
